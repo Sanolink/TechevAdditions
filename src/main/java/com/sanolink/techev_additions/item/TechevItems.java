f4824b96aaa3513bd0bb9494055a930664f58901
SanoLink <link4francais@gmail.com>
2025-05-04 22:20:37 +0200

    [✨] Tellurite / Chromatic / Evolium

--- src/main/java/com/sanolink/techev_additions/item/TechevItems.java
+++ src/main/java/com/sanolink/techev_additions/item/TechevItems.java
@@ -15,6 +15,9 @@ public class TechevItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TechevAdditions.MOD_ID);
     
     public static final RegistryObject<Item> TECHEVIUM = ITEMS.register("techevium", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB).rarity(Rarity.UNCOMMON)));
+    public static final RegistryObject<Item> TELLURITE= ITEMS.register("tellurite", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
+    public static final RegistryObject<Item> CHROMATIC = ITEMS.register("chromatic", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
+    public static final RegistryObject<Item> EVOLIUM = ITEMS.register("evolium", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
     public static final RegistryObject<Item> RAW_AURICARGENTUM = ITEMS.register("raw_auricargentum", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
     public static final RegistryObject<Item> RAW_CUPERZINATE = ITEMS.register("raw_cuperzinate", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
     public static final RegistryObject<Item> RAW_LEADOSNITE = ITEMS.register("raw_leadosnite", () -> new Item(new Item.Properties().tab(TechevCreativeModeTab.TECHEV_TAB)));
