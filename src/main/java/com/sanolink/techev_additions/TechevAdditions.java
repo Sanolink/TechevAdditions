f548f4858f283927565460a56c0a6fd88d6f60f6
SanoLink <link4francais@gmail.com>
2025-09-13 13:28:43 +0200

    - [✨] Made the "Regeneration" button movable

--- src/main/java/com/sanolink/techev_additions/TechevAdditions.java
+++ src/main/java/com/sanolink/techev_additions/TechevAdditions.java
@@ -12,6 +12,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -39,6 +40,7 @@ public class TechevAdditions
 
         MinecraftForge.EVENT_BUS.register(this);
 
+        context.registerConfig(ModConfig.Type.CLIENT, TechevAdditionsConfig.CLIENT_CONFIG);
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/com/sanolink/techev_additions/TechevAdditionsConfig.java
@@ -0,0 +1,28 @@
+package com.sanolink.techev_additions;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class TechevAdditionsConfig {
+
+    public static ForgeConfigSpec.IntValue regenButtonX;
+    public static ForgeConfigSpec.IntValue regenButtonY;
+
+    public static final ForgeConfigSpec CLIENT_CONFIG;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.comment("Configuration for TechEv Additions").push("general");
+
+        regenButtonX = builder
+                .comment("X position of Regenerations button")
+                .defineInRange("regenButtonX", 51, 0, 500);
+
+        regenButtonY = builder
+                .comment("Y position of Regenerations button")
+                .defineInRange("regenButtonY", 34, 0, 500);
+
+        builder.pop();
+        CLIENT_CONFIG = builder.build();
+    }
+}
