d6dd142b79139337789e6dde2e5186f0da5cc722
SanoLink <link4francais@gmail.com>
2026-06-28 22:19:51 +0200

    [✨] Added a config file to register custom Create Deco Coins

--- src/main/java/com/sanolink/techev_additions/TechevAdditionsConfig.java
+++ src/main/java/com/sanolink/techev_additions/TechevAdditionsConfig.java
@@ -1,6 +1,12 @@
 package com.sanolink.techev_additions;
 
+import com.google.gson.Gson;
 import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class TechevAdditionsConfig {
 
@@ -24,5 +30,28 @@ public class TechevAdditionsConfig {
 
         builder.pop();
         SERVER_CONFIG = builder.build();
+    }
+
+    //Create Deco Coins
+    private static List<String> coinMaterials;
+
+    public static List<String> getCoinMaterials() {
+        if (coinMaterials == null) coinMaterials = loadCoinMaterials();
+        return coinMaterials;
+    }
+
+    private static List<String> loadCoinMaterials() {
+        Path path = FMLPaths.CONFIGDIR.get().resolve("techev_additions-coins.json");
+        try {
+            if (Files.notExists(path)) {
+                Files.writeString(path, "[]");
+            }
+            String[] materials = new Gson().fromJson(Files.readString(path), String[].class);
+            TechevAdditions.LOGGER.info("[TechEv] Loaded coin materials: {}", (Object) materials);
+            return List.of(materials);
+        } catch (Exception e) {
+            TechevAdditions.LOGGER.error("[TechEv] Failed to read techev_coins.json, no coins added", e);
+            return List.of();
+        }
     }
 }
